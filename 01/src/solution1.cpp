fcb668d35ed63d85c4601a5e134cf3a3e5c89b8c
Your Name <tyler.g.anderson@vanderbilt@edu>
2019-02-11 16:44:39 -0600

    updated files yo

---
+++ 01/src/solution1.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+uint32_t exp(uint32_t n, uint32_t k) { 
+	uint32_t n_mod = n;
+	double x = log2(k);
+	if (k == 0) {
+		return 1;
+	} else if (k == 1) {
+		return n;
+	} else if (k == 2) {
+		return pow(n, 2);
+	} else if(x != floor(x)) {
+		for (int i = 0; i < floor(x); i++){
+			n_mod = pow(n_mod, 2);
+		}
+		return n_mod * exp(n, k-floor(x));
+	}
+	throw;
+}
+
+int main(int argc, char* argv[]) {
+	uint32_t n = *argv[1]; // read in argument for n */
+	cin >> n;
+	cout << exp(n, n); // compute n^n and write to stdout
+}
